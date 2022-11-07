d39e77d2e2c95270b48014ffe7eb67681e7bbb42
Zafer AYAN <ozcanzaferayan@gmail.com>
2022-11-07 14:37:06 +0300

    Add prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 80,
+    tabWidth: 4,
+    trailingComma: 'all',
+    singleQuote: true,
+    semi: true,
+    importOrder: ['^@components/(.*)$', '^@features/(.*)$', '^[./]'],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+};
--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,17 +1,19 @@
-import React, { useState } from "react";
-import Button from "@components/Button";
-import Container from "./components/CounterContainer";
-import Text from "./components/CounterText";
+import React, { useState } from 'react';
+
+import Button from '@components/Button';
+
+import Container from './components/CounterContainer';
+import Text from './components/CounterText';
 
 const Counter = () => {
-  const [count, setCount] = useState(0);
-  return (
-    <Container>
-      <Button down onClick={() => setCount(count - 1)} />
-      <Text text={count} />
-      <Button up onClick={() => setCount(count + 1)} />
-    </Container>
-  );
+    const [count, setCount] = useState(0);
+    return (
+        <Container>
+            <Button down onClick={() => setCount(count - 1)} />
+            <Text text={count} />
+            <Button up onClick={() => setCount(count + 1)} />
+        </Container>
+    );
 };
 
 export default Counter;
